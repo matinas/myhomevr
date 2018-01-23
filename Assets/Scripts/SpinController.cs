8cfae19b2fbd9e4e40ee5f21806013cc017683a6
Matias Nassi <matinas88@gmail.com>
2018-01-23 00:03:58 -0300

    First commit with the house perimeter walls on place and the living room almost completed

---
+++ Assets/Scripts/SpinController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SpinController : MonoBehaviour {
+
+	public int speed;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,speed,0);
+	}
+}
