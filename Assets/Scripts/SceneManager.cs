cc049339998f293441d75341c35542267ec134a3
Matias Nassi <matinas88@gmail.com>
2018-02-12 05:03:47 -0300

    Just a few modifications in the project hierarchy and some minor issues solved

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -8,12 +8,17 @@ namespace HomeVR {
 
 		[SerializeField] private Reticle m_Reticle;                         // The scene only uses SelectionSliders so the reticle should be shown.
 		[SerializeField] private SelectionRadial m_Radial;                  // Likewise, since only SelectionSliders are used, the radial should be hidden.
+		[SerializeField] private Transform m_mainCamera;
 
 		// Use this for initialization
 		void Awake () {
 			
 			m_Reticle.Show();
 			m_Radial.Hide();
+		
+			#if UNITY_ANDROID
+				m_mainCamera.position = new Vector3(m_mainCamera.position.x,m_mainCamera.position.y+1.6f,m_mainCamera.position.z);
+			#endif
 		}
 	}
 }
